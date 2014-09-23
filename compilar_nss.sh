b34b1db6ef8bae089490598ba52b07223f569ae2
Axel Díaz <diaz.axelio@gmail.com>
2014-09-23 14:52:22 -0430

    Añadiendo órdenes de generación .der a cada certificado

--- compilar_nss.sh
+++ compilar_nss.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env shell
 
 mkdir -p ~/public_html/certificados_iceweasel/certificados/
 mkdir -p ~/public_html/certificados_iceweasel/paquetes/
 cd ~/public_html/certificados_iceweasel/
 
 echo "Instalando dependencias necesarias"
-su -c "aptitude install git git-buildpackage build-essential quilt libc6-dev-i386"
+su -c "aptitude install git git-buildpackage build-essential quilt libc6-dev-i386 lib32z1-dev"
 
 echo " "
 echo "Iniciando procedimiento para obtencion del certificado"
@@ -22,7 +22,7 @@ cd ~/public_html/certificados_iceweasel/paquetes/
 rm -rf ~/public_html/certificados_iceweasel/get_certificados/
 apt-get source nss nspr
 
-cd nss-3.14.5/
+cd ~/public_html/certificados_iceweasel/paquetes/nss-3.14.5/
 export QUILT_PATCHES=debian/patches
 quilt pop -a
 
@@ -31,8 +31,55 @@ git init
 git add .
 git commit -a -m "Versión original del código fuente."
 
-cd mozilla
+cd ~/public_html/certificados_iceweasel/paquetes/nss-3.14.5/mozilla/
 ln -s ../../nspr-4.9.2/mozilla/nsprpub/ .
 
-cd security/nss/
-make nss_build_all BUILD_OPT=1
+COMMAND=`uname -r`
+echo $LIST
+ARCH="amd64"
+
+if echo "$COMMAND" | grep -q "$SOURCE"; then
+    cd ~/public_html/certificados_iceweasel/paquetes/nss-3.14.5/mozilla/security/nss/
+    make nss_build_all BUILD_OPT=1 USE_64=1
+
+    cd ~/public_html/certificados_iceweasel/paquetes/nss-3.14.5/mozilla/security/nss/cmd/addbuiltin/
+    make BUILD_OPT=1 USE_64=1
+
+    echo " "
+    echo "Se copiará Linux3.2_x86_64_glibc_PTH_64_OPT.OBJ a /usr/bin/"
+    echo "Se debe ejecutar como super usuario"
+    su -c "cp -v Linux3.2_x86_64_glibc_PTH_64_OPT.OBJ/addbuiltin /usr/bin/"
+else
+    cd ~/public_html/certificados_iceweasel/paquetes/nss-3.14.5/mozilla/security/nss/
+    make nss_build_all BUILD_OPT=1
+
+    cd ~/public_html/certificados_iceweasel/paquetes/nss-3.14.5/mozilla/security/nss/cmd/addbuiltin/
+    make BUILD_OPT=1
+
+    echo " "
+    echo "Se copiará Linux3.2_x86_glibc_PTH_OPT.OBJ a /usr/bin/"
+    echo "Se debe ejecutar como super usuario"
+    su -c "cp -v Linux3.2_x86_glibc_PTH_OPT.OBJ/addbuiltin /usr/bin/"
+fi
+
+
+echo " "
+echo "Convirtiendo los certificados con addbuiltin"
+cd ~/public_html/certificados_iceweasel/certificados/
+
+for certificado in `ls *.cer`;
+do openssl x509 -inform PEM -outform DER -in CERTIFICADO-RAIZ-SHA384.crt -out CERTIFICADO-RAIZ-SHA384.der
+
+
+CERTIFICADOS=`ls *.crt`
+
+for certificado in $CERTIFICADOS
+    do
+        nombre=`echo $certificado | cut -d \. -f 1`
+        openssl x509 -inform PEM -outform DER -in $certificado -out $nombre.der
+        comando=`openssl x509 -inform PEM -text -in $certificado | grep "Subject"`
+        O=`echo $comando| cut -d \O -f 2`
+        O=`echo $O| cut -d \= -f 2`
+        O=`echo $O| cut -d \, -f 1`
+        comando=`cat CERTIFICADO-RAIZ-SHA384.der | addbuiltin -n "$O" -t "C,C,C" > $nombre.nss`
+done
